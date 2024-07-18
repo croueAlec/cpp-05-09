caf87f6f7083219ed857fdc2a2ed1711c34c1df1
Alec Croue <alec.croue@gmail.com>
2024-07-18 16:28:22 +0200

    tests sh

---
+++ 9CPP/ex01/tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+execute_rpn() {
+	echo ""
+	echo "$1"
+	./RPN "$1"
+}
+
+make cre
+
+execute_rpn "8 9 * 9 - 9 - 9 - 4 - 1 +"
+execute_rpn "7 7 * 7 -"
+execute_rpn "1 2 * 2 / 2 * 2 4 - +"
+execute_rpn "(1 + 1)"
